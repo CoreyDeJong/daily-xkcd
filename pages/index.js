73df92be4339b1d9bff0f124cda19f00fd6d279d
Corey DeJong <dejongcorey@gmail.com>
2020-07-06 18:05:11 -0700

    can't get next/link to work, used demo code and still wont work

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,58 @@
-export default function Home() {
+import Link from 'next/Link'
+import MyLayout from '../components/MyLayout'
+
+
+export default function Home(props) {
   return (
-    <div className="hello">
-      <p>Hello World</p>
-      <style jsx>{`
-        .hello {
-          font: 15px Helvetica, Arial, sans-serif;
-          background: #eee;
-          padding: 100px;
-          text-align: center;
-          transition: 100ms ease-in background;
-        }
-        .hello:hover {
-          background: #ccc;
-        }
-      `}</style>
-    </div>
+    <MyLayout>
+        <h1>XKCD</h1>
+      <h2>{props.comic.title}</h2>
+      <img src={props.comic.img} alt={props.comic.alt} />
+      <Footer comicNum={props.comic.num} />
+    </MyLayout>
   )
 }
+
+function Footer(props) {
+
+    const currentNum = props.comicNum;
+
+    const nums = [];
+
+    for(let n = currentNum; n > currentNum - 10; n--) {
+        nums.push(n)
+    }
+
+return (
+    <footer>
+        <h2>Previous {nums.length}</h2>
+        <ul>
+            {nums.map(num => (
+                // <li key={num}>
+                    <Link href="/num/[id].js" as={`/num/${num}`} key={num}>
+                        <a>#{num}</a>
+                    </Link>
+                // </li>
+            ))}
+        </ul>
+        <style jsx>{`
+            a {
+                margin-right: 5px;
+            }
+        `}</style>
+    </footer>
+)
+}
+
+export async function getServerSideProps(context) {
+
+    const response = await fetch('https://xkcd.com/info.0.json');
+
+    const data = await response.json();
+
+    return {
+      props: {
+          comic: data
+      }, // will be passed to the page component as props
+    }
+  }
